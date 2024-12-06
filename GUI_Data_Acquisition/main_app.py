7d6614afebfdcf1a92e4e84684ffb81b567ac348
Arash Keshavarz <Arashkeshavarzx@gmail.com>
2024-12-06 23:37:12 +0100

    Adjusted features

--- GUI_Data_Acquisition/main_app.py
+++ GUI_Data_Acquisition/main_app.py
@@ -11,35 +11,37 @@ class MainApp(ctk.CTk):
         self.title("EIT/Isokinetic Measurement System")
         self.geometry("800x600")
         self.grid_columnconfigure((0, 1), weight=1)  # Two columns with equal weight
-        self.grid_rowconfigure((0, 1), weight=1)  # One row
+        #self.grid_rowconfigure((0, 1), weight=1)  
 
         ctk.set_appearance_mode("dark")
-        ctk.set_default_color_theme("blue")  # Set color theme
+        ctk.set_default_color_theme("dark-blue")  # Set color theme
 
 
         # Left Frame: IsokineticMeasurementModule
         self.left_frame = IsokineticMeasurementModule(self)
-        self.left_frame.frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")  # Place in left column
+        self.left_frame.frame.grid(row=0, column=0, padx=20, pady=(10, 0), sticky="new")  # Place in left column
 
         # Right Frame: EITMeasurementModule
         self.right_frame = EITMeasurementModule(self)
-        self.right_frame.frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")  # Place in right column
+        self.right_frame.frame.grid(row=0, column=1, padx=20, pady=(10, 0), sticky="new")  # Place in right column
 
         ################################################################
         # Button to Generate The Protocol
 
         self.protocol_button = ctk.CTkButton(self, text="Generate Protocol", command=self.generate_protocol)
-        self.protocol_button.grid(row=1, column=0, columnspan=2, pady=20)
-        
+        self.protocol_button.grid(row=1, column=0, padx=20,pady=(0,40), sticky="new")
+        # needed to be fixed #
 
 
 
     def generate_protocol(self):
         # Get data from IsokineticMeasurementModule and EITMeasurementModule
         participant_name = self.left_frame.get_participant_name()
+        participant_age = self.left_frame.get_participant_age()
+        participant_gender = self.left_frame.get_participant_gender()
         participant_leg = self.left_frame.get_participant_leg()
 
-        if not participant_name or not participant_leg:
+        if not participant_name or not participant_leg or not participant_gender or not participant_age:
             CTkMessagebox(title="Error", message="Please complete all participant details before generating the protocol.", icon="cancel")
             return
         
@@ -58,8 +60,12 @@ class MainApp(ctk.CTk):
         # Add participant details section
         protocol.add_section(
             "Participant Details",
-            f"Name: {participant_name}\nLeg: {participant_leg}",
+            f"Name: {participant_name}\n"
+            f"Age: {participant_age}\n"
+            f"Gender: {participant_gender}\n"
+            f"Leg: {participant_leg}"
         )
+
 
         # Add Isokinetic Measurement section
         protocol.add_section(
