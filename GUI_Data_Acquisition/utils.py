d22ed1a0d0143fda3de7dcaf79e1987c7478ea9d
Arash <125802011+Arash-Keshavarz@users.noreply.github.com>
2025-02-10 19:11:12 +0100

    Create Subdirectories (#2)

--- GUI_Data_Acquisition/utils.py
+++ GUI_Data_Acquisition/utils.py
@@ -1,20 +1,25 @@
 import os
 
-def create_participant_directory(participant_name):
+def create_participant_directory(participant_num: str) -> tuple[str, str]:
     """
-    Create a directory structure for storing participant data.
+    Creates a structured directory for each participant within the 'measurements' folder.
+    
+    This function ensures the existence of:
+    - A participant-specific directory inside 'measurements'.
+    - Two subdirectories: 'eit_raw' and 'iso_raw'.
+    
+    Parameters:
+        participant_name (str): The name or identifier of the participant.
+
+    Returns:
+        tuple[str, str]: Paths to the 'eit_raw' and 'iso_raw' directories.
     """
-    # Define the base directory
-
-    base_dir = "measurements"
-
-    if not os.path.exists(base_dir):
-        os.makedirs(base_dir)
+    base_dir = "Final_Measurements"
+    participant_dir = os.path.join(base_dir, participant_num)
     
-    # Define the participant's directory path
-    participant_dir = os.path.join(base_dir, f"Participant_{participant_name}")
+    # Create ISO and EIT subdirectories #
+    for sub_dir in ["", "eit_raw", "iso_raw"]:
+        os.makedirs(os.path.join(participant_dir, sub_dir), exist_ok=True)
     
-    if not os.path.exists(participant_dir):
-        os.makedirs(participant_dir)
+    return os.path.join(participant_dir, "eit_raw"), os.path.join(participant_dir, "iso_raw")
 
-    return participant_dir
